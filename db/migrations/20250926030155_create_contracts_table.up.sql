fd28e75e8069b782f4c2c9f75cde1c0fc9b2758f
ayas <ahmdyaasiin@student.ub.ac.id>
2025-09-27 02:26:21 +0700

    feat(db): add schema for contracts table

---
+++ db/migrations/20250926030155_create_contracts_table.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE contracts (
+    id UUID PRIMARY KEY,
+    human_id CHAR(15) NOT NULL UNIQUE,
+    email VARCHAR(60) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    notes TEXT,
+    risk_level SMALLINT NOT NULL DEFAULT 0,
+    status SMALLINT NOT NULL DEFAULT 0,
+    application_status SMALLINT NOT NULL DEFAULT 0,
+    category SMALLINT NOT NULL,
+    start_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    end_date TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_contracts_title ON contracts(title);
+CREATE INDEX idx_contracts_company ON contracts(company);
