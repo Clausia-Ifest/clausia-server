54223e37b0f5eff06649b05993e282941e64c9a5
ayas <ahmdyaasiin@student.ub.ac.id>
2025-09-27 07:16:10 +0700

    feat: add contract for feature

--- internal/domain/contract/contract.go
+++ internal/domain/contract/contract.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Clausia-Ifest/clausia-server/internal/domain/dto"
 	"github.com/Clausia-Ifest/clausia-server/internal/domain/entity"
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -13,10 +14,14 @@ type IRContract interface {
 	CreateRelation(ctx context.Context, ext sqlx.ExtContext, data entity.ContractDocument) error
 	All(ctx context.Context, ext sqlx.ExtContext, conditions dto.GetContractParams) ([]entity.Contract, int64, error)
 	Update(ctx context.Context, ext sqlx.ExtContext, data *entity.Contract) error
+	Get(ctx context.Context, ext sqlx.ExtContext, conditions dto.GetContractParams) (*entity.Contract, error)
+	CreateChatHistory(ctx context.Context, ext sqlx.ExtContext, data entity.Chat) error
 }
 
 type IUContract interface {
 	Submit(ctx context.Context, req dto.SubmitContractRequest) error
 	All(ctx context.Context, req dto.AllContractsRequest) (*dto.AllContractResponse, error)
 	Update(ctx context.Context, req dto.UpdateContractRequest) error
+	Get(ctx context.Context, id uuid.UUID) (*dto.Contract, error)
+	Chat(ctx context.Context, req dto.ChatRequest) (string, error)
 }
